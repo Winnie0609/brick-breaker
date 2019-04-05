32cbd5ae2de2a21d6341bc878e47b02a7800e973
Winnie0609 <49323767+Winnie0609@users.noreply.github.com>
2019-04-06 04:29:21 +0800

    Add files via upload

---
+++ Brick-breaker.js
@@ -0,0 +1,103 @@
+var bar, bricks
+
+let playerScore = 0
+
+function setup () {
+  createCanvas(window.innerWidth, window.innerHeight)
+  bar = createSprite(width / 2, height - 100, 150, 15)
+  bar.shapeColor = color(255)
+  bar.immovable = true
+
+  wallTop = createSprite(1, +40, 5000, 1) 
+  wallTop.shapeColor = color(0)
+  wallTop.immovable = true
+  wallRight = createSprite(-1, -1, 1, 5000 )
+  wallRight.shapeColor = color(0)
+  wallRight.immovable = true;
+  wallLeft = createSprite(width, -100, 10, 5000 )
+  wallLeft.shapeColor = color(0)
+  wallLeft.immovable = true
+  wallbottom = createSprite(width/2,height,width,10)
+  wallbottom.immovable=true
+  wallbottom.shapeColor = color(0)
+
+  generatebricks()
+
+  ball = createSprite(width / 2, height - 150, 15, 15)
+  ball.shapeColor = color(0, 0, 250)
+}
+
+function generatebricks (){
+bricks = new Group()
+  let x = 8, y = 8, margin = 80, gap = 10
+  let size = {
+    x: (width - margin * 2 - gap * (x - 1)) / x,
+    y: 25
+  }
+  for (var i = 0; i < x; i++) {
+    for (var j = 0; j < y; j++) {
+      let position = {
+        x: margin + (size.x + gap) * i + size.x / 2,
+        y: margin + (size.y + gap) * j + size.y / 2
+      }
+      let brick = createSprite(position.x, position.y, size.x, size.y)
+      brick.shapeColor = color(255)
+      brick.immovable = true
+      bricks.add(brick)
+    }
+  }
+}
+var bar, bricks, fire = false
+function draw () {
+  background(0)
+
+  bar.position.x = mouseX
+  if (!fire) {
+    ball.position.x = mouseX
+  }
+
+  ball.bounce(bar,hitbar)
+
+  ball.bounce(bricks, hitBrick)
+  ball.bounce(wallTop)
+  ball.bounce(wallRight)
+  ball.bounce(wallLeft)
+  ball.bounce(wallbottom, hitonwallbottom )
+
+  let c = color(0, 0, 100); 
+  fill(c)
+  noStroke()
+  rect(0, 0, window.innerWidth,40 );
+  
+   fill(200);
+   textSize(20);
+   text(`Score:${playerScore}`, width/2, 30);
+   
+  drawSprites()
+}
+
+function hitbar(){
+    ball.setSpeed(10, ball.getDirection() + (ball.position.x-bar.position.x)*0.5)
+} 
+
+function mousePressed () {
+  if (!fire) {
+    fire = true
+    ball.setSpeed(10, -70)
+  }
+}
+
+function hitBrick (ball, brick) {
+  brick.remove()
+  playerScore += 100
+}
+
+function hitonwallbottom(ball, wallbottom){
+    console.log('asd')
+    bricks.removeSprites()
+    generatebricks()
+    fire = false
+    ball.setSpeed(0)
+    ball.position.y = height - 150
+    playerScore = 0
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+brick breaker with pg.js
+
+*[] Create index.html
+*[] Add.pg.js
+*[] set up basic pg.js
+*[] Display score
+*[] Get a paddle on the screen
+*[] Move the paddle with mouse 26:30
+*[] Get a ball on the screen
+*[] Launch the ball when the game starts
+*[] bounch ball off of edges
+*[] bounch ball off paddle
+*[] create 8 row of bricks
+*[] Ball bounces off brick
+    *[] Brick is destroyed
+    *[] Score increases by 1
+*[] Game is over when all bricks are destroyed
+*[] Game over when ball is dropped
+
+### Stretch
+*[] Each brick broken is 1 point
+*[] Each row of bricks is worth +1 points as it goes up
+*[] Harder bricks take more than one bounce to destroy
+*[] Increase speed with every brick that is broken
+*[] 3 lives,game over
